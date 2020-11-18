89cdd5f1028bf2e9be57e3b50d0a8f67c21fbffe
Nova Z Hart <Nova.Z.Hart@gmail.com>
2020-11-17 19:38:35 -0500

    Better colors for target HP and remove cross for planet

--- TargetDisplay.gd
+++ TargetDisplay.gd
@@ -1,11 +1,11 @@
 extends Node2D
 
 var structure_have: Color = Color(1,0.2,0.2,1.0)
-var structure_lack: Color = Color(0.4,0,0,1.0)
+var structure_lack: Color = Color(0.4,0.2,0.2,1.0)
 var hull_have: Color = Color(0.9,0.7,0.1,1.0)
 var hull_lack: Color = Color(0.5,0.3,0.0,1.0)
 var shields_have: Color = Color(0.4,0.4,1.0,1.0)
-var shields_lack: Color = Color(0,0,0.5,1.0)
+var shields_lack: Color = Color(0.1,0.1,0.5,1.0)
 
 func player_target_deselect(var system):
 	system.disconnect('player_target_deselect',self,'player_target_deselect')
@@ -42,7 +42,7 @@ func _draw():
 		circle_color = Color(1,1,1,0.4)
 		cross_color = Color(1,1,1,1)
 		circle_width = 8.0
-		show_cross = true
+		show_cross = false
 	else:
 		# Circle whole bounding box for jagged things like ships
 		circle_width = 1.5
@@ -53,7 +53,6 @@ func _draw():
 		show_cross = false
 	var out2d: Vector2 = camera.unproject_position(out)
 	var radius: float = pos2d.distance_to(out2d)
-	draw_arc(pos2d,radius+5.0,0.0,2*PI,240,circle_color,circle_width,true)
 	if show_cross:
 		draw_line(Vector2(pos2d.x+radius/2,pos2d.y),Vector2(pos2d.x+radius*1.1+5.0,pos2d.y),cross_color,cross_width,true)
 		draw_line(Vector2(pos2d.x-radius/2,pos2d.y),Vector2(pos2d.x-radius*1.1-5.0,pos2d.y),cross_color,cross_width,true)
@@ -64,3 +63,5 @@ func _draw():
 		draw_hp_arc(pos2d,shields_have,shields_lack,radius+6.5,0,2*PI/3,target.shields,target.max_shields)
 		draw_hp_arc(pos2d,hull_have,hull_lack,radius+6.5,2*PI/3,4*PI/3,target.hull,target.max_hull)
 		draw_hp_arc(pos2d,structure_have,structure_lack,radius+6.5,4*PI/3,2*PI,target.structure,target.max_structure)
+	else:
+		draw_arc(pos2d,radius+5.0,0.0,2*PI,240,circle_color,circle_width,true)
