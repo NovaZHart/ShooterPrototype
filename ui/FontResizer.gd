2208480622b9403194ea705f50dc2114a1b1b24b
Nova Z Hart <nova.z.hart@gmail.com>
2022-04-30 01:02:53 -0400

    Remove prints from FontResizer

--- ui/FontResizer.gd
+++ ui/FontResizer.gd
@@ -23,12 +23,12 @@ func update_node_property_font_sizes(parent: Node, font_size: float,property_nam
 	for property_name in property_names:
 		var font = parent.get(property_name)
 		if not font:
-			print(parent.get_path(),' has no property ',property_name)
+#			print(parent.get_path(),' has no property ',property_name)
 			continue
 		if not (font is Font):
-			print(parent.get_path(),' property is not a font')
+#			print(parent.get_path(),' property is not a font')
 			continue
-		print('Setting ',parent.get_path(),' ',property_name,' property font size to ',font_size)
+#		print('Setting ',parent.get_path(),' ',property_name,' property font size to ',font_size)
 		font.size=font_size
 
 func update_node_font_sizes(parent: Node,font_size: float,font_names: PoolStringArray):
@@ -36,9 +36,9 @@ func update_node_font_sizes(parent: Node,font_size: float,font_names: PoolString
 		for font_name in font_names:
 			var font: Font = parent.get_font(font_name)
 			if not font:
-				print(parent.get_path(),' node has no font ',font_name)
+#				print(parent.get_path(),' node has no font ',font_name)
 				continue
-			print('Setting ',parent.get_path(),' font ',font_name,' size to ',font_size)
+#			print('Setting ',parent.get_path(),' font ',font_name,' size to ',font_size)
 			font.size=font_size
 
 func update_node_theme_default_font_size(parent: Node,font_size: float):
@@ -49,25 +49,25 @@ func update_node_theme_default_font_size(parent: Node,font_size: float):
 		var font: Font = theme.default_font
 		if not font:
 			return
-		print('Setting ',parent.get_path(),' theme default font size to ',font_size)
+#		print('Setting ',parent.get_path(),' theme default font size to ',font_size)
 		font.size=font_size
 
 func update_node_theme_all_fonts_size(parent: Node,font_size: float):
 	if parent.has_method('get_theme'):
 		var theme: Theme = parent.get_theme()
 		if not theme:
-			print(parent.get_path(),' node has no theme')
+#			print(parent.get_path(),' node has no theme')
 			return
 		var type: String = parent.get_class()
 		var font_names: PoolStringArray = theme.get_font_list(type)
-		if not font_names:
-			print(parent.get_path(),' node has no font names')
+#		if not font_names:
+#			print(parent.get_path(),' node has no font names')
 		for font_name in font_names:
 			var font: Font = theme.get_font(font_name,type)
 			if not font:
-				print(parent.get_path(),' node has no font ',font_name)
+#				print(parent.get_path(),' node has no font ',font_name)
 				continue
-			print('Setting ',parent.get_path(),' theme type ',type,' font ',font_name,' size to ',font_size)
+#			print('Setting ',parent.get_path(),' theme type ',type,' font ',font_name,' size to ',font_size)
 			font.size=font_size
 
 func update_font_size():
