04c24482daf1b3ad477f996e376cade8a1bd7b1f
Nova Z Hart <nova.z.hart@gmail.com>
2022-06-07 10:25:36 -0400

    move meshes => effects/meshes

--- ships/ShipStats.gd
+++ ships/ShipStats.gd
@@ -52,7 +52,7 @@ export var base_power: float = -1
 
 export var ai_type: int = 0 setget set_ai_type
 
-export var cargo_puff: Mesh = preload('res://meshes/cargo-puff.mesh');
+export var cargo_puff: Mesh = preload('res://effects/meshes/cargo-puff.mesh');
 export var cargo_web_add_radius: float = 3
 export var cargo_web_strength: float = -1
                                                                #  0    1    2    3    4    5    6    7    8    9    10   11   12
