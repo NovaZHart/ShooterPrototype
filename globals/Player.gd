15a21991a1f195e08d3dbccdc0eccbdd1b73edb0
nova.z.hart <nova.z.hart@gmail.com>
2022-08-27 17:33:30 -0400

    remove Asteroid Test and start at Kindra

--- globals/Player.gd
+++ globals/Player.gd
@@ -1,7 +1,7 @@
 extends Node
 
 const PLAYER_START_SHIP_DESIGN: String = 'thoroughbred_haruspex'
-const PLAYER_START_LOCATION: String = '/root/systems/seti-gamma/test'
+const PLAYER_START_LOCATION: String = '/root/systems/seti-gamma/kindra'
 
 var player_ship_design
 var system setget set_system,get_system
